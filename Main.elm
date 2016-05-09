c0d3293d0e71e519fd6b71b2391af6077762b9ed
Jeffrey Massung <massung@gmail.com>
2016-05-09 11:08:19 -0500

    Missed Main.elm.

--- Main.elm
+++ Main.elm
@@ -9,33 +9,38 @@ import HN exposing (..)
 import Story exposing (..)
 import View exposing (..)
 
+{-| The Model is just a list of stories. -}
 type alias Model =
     { stories : List Story
     }
 
--- model update actions
+{-| Interface and mailbox signals combine into Actions. -}
 type Action
     = Refresh (List Story)
 
--- the final view of all the hn stories
+{-| The final, aggregated model transformed to HTML. -}
 main : Signal Html.Html
 main = 
     let stories = Signal.foldp update (Model []) aggregate in
-    Signal.map (viewStories << rankedStories) stories
+    Signal.map (view << viewStories << rankedStories) stories
 
--- every minute get the top stories
+{-| Wrap a list of rendered Stories into a parent HTML element. -}
+view : List Html.Html -> Html.Html
+view = Html.body []
+
+{-| Every minute, get the top 30 stories from HN. -}
 port latest : Signal (Task.Task Http.Error ())
 port latest = Signal.map (HN.topStories 30) <| every minute
 
--- whenever we refetch the stories, perform a refresh
+{-| Create a Refresh action each time the latest stories change. -}
 aggregate : Signal Action
 aggregate = Signal.map Refresh stories
 
--- return the list of items, sorted by page rank
+{-| Sort all the stories in the model based on their rank. -}
 rankedStories : Model -> List Story
 rankedStories model = List.sortBy (\s -> s.rank) model.stories
 
--- aggregate stories and handle interface
+{-| Updates the current Model given an Action. -}
 update : Action -> Model -> Model
 update action model =
     case action of
