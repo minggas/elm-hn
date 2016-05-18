ec400b0c971c59e4208022021a58ec7682a02db1
Jeffrey Massung <massung@gmail.com>
2016-05-18 08:02:14 -0500

    Using electron 1.0.2 until 1.1.0 is fixed (requires vs2015 be installed).

--- build_win32.bat
+++ build_win32.bat
@@ -5,8 +5,13 @@ set app=hacker-news-reader
 set platform=win32
 set arch=x64
 set icon=app.ico
-set ignore='\.elm$'
-set electron=1.1.0
+set copyright="Copyright (c) 2016, Jeffrey Massung"
+set ignore="elm-stuff/|\.(elm|gitignore)$"
+set electron=1.0.2
+
+rem windows only settings
+set CompanyName="Jeffrey Massung"
+set ProductName="Hacker News | reader"
 
 rem final output location
 set out=".\out\%app%-%platform%-%arch%"
@@ -15,7 +20,7 @@ rem compile the app
 elm-make Main.elm --output=elm.js
 
 rem build the executable
-call electron-packager . %app% --platform=%platform% --arch=%arch% --icon=%icon% --ignore=%ignore% --overwrite --out=out --version=%electron%
+call electron-packager . %app% --platform=%platform% --arch=%arch% --icon=%icon% --ignore=%ignore% --overwrite --out=out --version=%electron% -- app-copyright=%copyright% --version-string.CompanyName=%CompanyName% --version.string.ProductName=%ProductName%
 
 rem rename the executable
 ren "%out%\%app%.exe" "Hacker News reader.exe"
